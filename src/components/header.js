25707cea2a9eb7e661476e4060280099bc0e0d47
wesleybertipaglia <wesleybertipaglia@gmail.com>
2023-11-03 19:33:03 -0300

    fix: nav links

--- src/components/header.js
+++ src/components/header.js
@@ -5,10 +5,10 @@ export default function Header() {
         <a href="/index.html">Home</a>
 
         <ul class="d-flex gap-2 list-unstyled m-0">
-            <li><a href="/pages/about.html">Aboout</a></li>
-            <li><a href="/pages/contact.html">Contact</a></li>
-            <li><a href="/pages/projects.html">Projects</a></li>
-            <li><a href="/blog/">Blog</a></li>
+            <li><a href="pages/about.html">Aboout</a></li>
+            <li><a href="pages/contact.html">Contact</a></li>
+            <li><a href="pages/projects.html">Projects</a></li>
+            <li><a href="blog/">Blog</a></li>
         </ul>
     </nav>
     `
