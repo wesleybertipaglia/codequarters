c33c74c16993aaf9aa942274ee873dc7e7b5505d
wesleybertipaglia <wesleybertipaglia@gmail.com>
2023-11-03 22:52:58 -0300

    fix: pages url

--- src/components/links.js
+++ src/components/links.js
@@ -1,20 +1,22 @@
+var root = window.location.host
+
 export default function Links() {
     const links = {
-        home: { name: 'Home', url: '/index.html' },
+      home: { name: 'Home', url: `/index.html` },
 
-        nav: 
-        [
-          { name: 'About', url: 'pages/about.html' },
-          { name: 'Contact', url: 'pages/contact.html' },
-          { name: 'Projects', url: 'pages/projects.html' },
-          { name: 'Blog', url: 'blog/' },
-        ],
+      nav: 
+      [
+        { name: 'About', url: `${root}/pages/about.html` },
+        { name: 'Contact', url: `${root}/pages/contact.html` },
+        { name: 'Projects', url: `${root}/pages/projects.html` },
+        { name: 'Blog', url: `${root}/blog` },
+      ],
 
-        footer: 
-        [
-          { name: 'Privacy Policy', url: 'pages/privacy-policy.html' },
-          { name: 'Terms of Service', url: 'pages/terms-of-service.html' },
-        ],          
+      footer: 
+      [
+        { name: 'Privacy Policy', url: 'pages/privacy-policy.html' },
+        { name: 'Terms of Service', url: 'pages/terms-of-service.html' },
+      ],          
     }
 
     return links;
