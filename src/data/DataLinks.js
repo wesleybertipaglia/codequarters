57065ea5934560204ea0cf2523033a8f9e4c4712
wesleybertipaglia <wesleybertipaglia@gmail.com>
2023-11-19 18:24:00 -0300

    fix: add env

--- src/data/DataLinks.js
+++ src/data/DataLinks.js
@@ -1,11 +1,13 @@
+import BaseUrl from "/config/env.js"
+
 const DataLinks = {
-    home: { name: 'Home', url: `/` },
+    home: { name: 'Home', url: `${BaseUrl}` },
     nav: 
     [
-      { name: 'About', url: `/pages/about.html` },
-      { name: 'Contact', url: `/pages/contact.html` },
-      { name: 'Projects', url: `/pages/projects.html` },
-      { name: 'Blog', url: `/blog` },
+      { name: 'About', url: `${BaseUrl}/pages/about.html` },
+      { name: 'Contact', url: `${BaseUrl}/pages/contact.html` },
+      { name: 'Projects', url: `${BaseUrl}/pages/projects.html` },
+      { name: 'Blog', url: `${BaseUrl}/blog` },
     ],        
 }
 export default DataLinks
